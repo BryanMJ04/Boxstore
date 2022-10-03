7a4e5aa4e7a51e6cada40f152c7c617cc82d0427
BryanMJ04 <109931931+BryanMJ04@users.noreply.github.com>
2022-10-03 12:16:19 -0600

    practica02

---
+++ src/main/java/com/boxstore/BoxstoreApplication.java
@@ -0,0 +1,13 @@
+package com.boxstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boxstore/BoxstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
